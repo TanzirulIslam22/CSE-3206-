5edd26c0b83b201c2dad3a92e60b85ef7862cbd9
Hi-There-shuvo <saddamshuvo1710@gmail.com>
2026-07-28 11:54:18 +0600

    Commited by Shuvo

--- Lab01-GitAndGithub/Tanzirul-2203054.cpp
+++ Lab01-GitAndGithub/Tanzirul-2203054.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 using namespace std;
 
+int fact(int a);
+
 int main()
 {
   int w;
   cin >> w;
 
-  if (w > 2 && w % 2 == 0)
-    cout << "YES";
+  if (w > 2 && w % 2 == 0){
+    cout << "YES"<< endl;
+    int a = fact(w);
+    cout <<"The factorila is = "<< a << endl ;
+  }
   else
     cout << "NO";
 
   return 0;
 }
+
+
+int fact(int a){
+  int result = 1 ;
+
+  for(int i = a ; i > 0 ; i--){
+    result *= i;
+  }
+
+  return result;
+}
+
