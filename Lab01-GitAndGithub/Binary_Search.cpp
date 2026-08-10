281d98e911d59032ecdb04526b6994cce55db210
Hi-There-shuvo <saddamshuvo1710@gmail.com>
2026-08-10 15:46:27 +0600

    Update Binary Search prompt text

--- Lab01-GitAndGithub/Binary_Search.cpp
+++ Lab01-GitAndGithub/Binary_Search.cpp
@@ -20,7 +20,7 @@ int binarySearch(int arr[], int n, int target) {
 
 int main() {
     int n;
-    cout << "Enter the number of elements: ";
+    cout << "Enter the number of elements of an array: ";
     cin >> n;
 
     int arr[n];
