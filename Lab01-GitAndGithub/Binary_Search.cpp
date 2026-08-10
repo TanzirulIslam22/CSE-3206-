70c4159d4394b935594a05f2bbeead85fb990b9c
Hi-There-shuvo <saddamshuvo1710@gmail.com>
2026-08-10 15:37:45 +0600

    Added Binary Search

---
+++ Lab01-GitAndGithub/Binary_Search.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return -1; // Element not found
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " sorted elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int target;
+    cout << "Enter the element to search: ";
+    cin >> target;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1)
+        cout << "Element found at index: " << result << endl;
+    else
+        cout << "Element not found in the array." << endl;
+
+    return 0;
+}
